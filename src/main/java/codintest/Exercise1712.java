594bc9c3dfb2126049e2b230fab0de7aeb84c836
blackdooyu <84924161+blackdooyu@users.noreply.github.com>
2021-12-30 20:35:48 +0900

    1712

---
+++ src/main/java/codintest/Exercise1712.java
@@ -0,0 +1,41 @@
+package main.java.codintest;
+
+import java.util.Scanner;
+
+/**
+ 백준 1712번 손익분기점 문제
+ **/
+
+public class Exercise1712 {
+
+   public static int result(int fixPrice,int totalPrice,int count) {
+
+        while (fixPrice >= 0){
+
+            fixPrice -= totalPrice;
+            count++;
+
+        }
+
+        return count;
+   }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int fixPrice = sc.nextInt(); //고정 지출
+        int eachPrice = sc.nextInt(); // 생산 할때마다 지출
+        int productPrice = sc.nextInt(); // 상품 가격
+
+        if(eachPrice >= productPrice){
+            System.out.println("-1");
+            return;
+        }
+
+        int result = Exercise1712.result(fixPrice, productPrice - eachPrice, 0);
+
+        System.out.println(result);
+
+    }
+}
