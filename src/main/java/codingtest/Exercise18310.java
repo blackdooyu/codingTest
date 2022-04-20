21279dd0ad20dc8e37324fe92418b0c56689ea47
blackdooyu <84924161+blackdooyu@users.noreply.github.com>
2022-04-20 16:06:31 +0900

    18310번

---
+++ src/main/java/codingtest/Exercise18310.java
@@ -0,0 +1,31 @@
+package main.java.codingtest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 18310번 안테나
+ */
+public class Exercise18310 {
+
+    static int arr[];
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[(n-1)/2]);
+
+    }
+}
